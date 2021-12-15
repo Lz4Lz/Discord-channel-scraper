887a2be6b008a0f1fc7d292c8a60db4b1eaa0fb6
Lz4Lz <59481770+Lz4Lz@users.noreply.github.com>
2021-12-15 20:12:15 +0100

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,57 @@
+import requests
+
+token = input("Token: ") #discord token
+channel = input("Channel ID: ")
+
+option = input("[1] Images\n[2]Gifs\n[3]Videos\n[4] All\n\n-->\t")
+
+headers = {
+'Authorization': f'{token}'
+}
+
+r = requests.get(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers)
+
+data = r.json()
+
+try:
+	keys = data[:]
+except Exception:
+	print("Wrong ChannelID or Discord Token. (or discord api related errors)")
+	exit()
+
+def images(data):
+	for key in keys:
+		values = key['attachments']
+		for value in values:
+			print(value['url'], file=open("output.txt", "a")) #outputs to output.txt. If u plan to use this again rename the old output.txt or move it to another folder.
+
+def gifs(data):
+	for gif in keys:
+		gifs = gif['content']
+		if ".gif" in gifs: 
+			print(gifs, file=open("output.txt", "a"))
+
+def videos(data):
+	for video in keys:
+		videos = video['content']
+		if ".mp4" in videos: 
+			print(videos, file=open("output.txt", "a"))
+
+def options(option):
+	if option == '1':
+		images(data)
+	elif option == '2':
+		gifs(data)
+	elif option == '3':
+		videos(data)
+	elif option == '4':
+		images(data)
+		gifs(data)
+		videos(data)
+	else:
+		exit()
+
+options(option)
+
+if __name__ == "__main__":
+	exit()
